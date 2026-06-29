6a7ee87735b34b742086e865eee7a6c77d7f9775
Takagi Kensuke <janus.wel.3@gmail.com>
2026-06-29 15:24:25 +0900

    🔧🍱 add settings to host on CfWorkers / add 404 page

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+if [ -n "$CI" ]; then
+  ZOLA_VERSION="${ZOLA_VERSION:-0.20.0}"
+  curl -sL "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz" | tar xz
+  export PATH="$PWD:$PATH"
+fi
+
 cd tools/embed-gen
 npm clean-install
 npm run fetch
